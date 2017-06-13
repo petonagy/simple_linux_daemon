2e0637652e68d1044e299edfd782a5bad54a424a
Peter Nagy <xnagyp01@stud.fit.vutbr.cz>
2017-06-13 23:10:54 +0200

    ADD: Compute cpu usage from /proc/stat file

--- daemon.c
+++ daemon.c
@@ -35,16 +35,75 @@
 /* Global variables */
 int listenfd = 0, connfd = 0;
 
-/* Memory info - parsed from /proc/meminfo */
-struct sys_mem_info {
-    unsigned int mem_total;
-    unsigned int mem_free;
-    unsigned int mem_available;
-    unsigned int mem_buffered;
-    unsigned int mem_cached;
+/* CPU usage info - parsed from /proc/stat */
+struct sys_cpu_info {
+    unsigned long long user;
+    unsigned long long nice;
+    unsigned long long system;
+    unsigned long long idle;
+    unsigned long long iowait;
+    unsigned long long irq;
+    unsigned long long softirq;
+    unsigned long long steal;
+    unsigned long long guest;
+    unsigned long long guest_nice;
 };
 
-/* Get memory usage number from a line in kB */
+/* Memory info - parsed from /proc/meminfo */
+struct sys_mem_info {
+    unsigned long mem_total;
+    unsigned long mem_free;
+    unsigned long mem_available;
+    unsigned long mem_buffered;
+    unsigned long mem_cached;
+};
+
+
+/**
+ * @brief      Gets the total CPU usage percentage computed from /proc/stat file
+ * @return     The total CPU usage
+ */
+char *get_cpu_usage()
+{
+    char *result;
+    struct sys_cpu_info cpuinfo;
+    FILE *cpu_f = fopen("/proc/stat", "r");
+    if (cpu_f == NULL) {
+        perror("Could not open /proc/stat file");
+        return NULL;
+    }
+
+    /* Read first line of the file */
+    char buffer[1024];
+    char* ret = fgets(buffer, sizeof(buffer) - 1, cpu_f);
+    if (ret == NULL) {
+        perror("Could not read /proc/stat file");
+        fclose(cpu_f);
+        return NULL;
+    }
+    fclose(cpu_f);
+
+    sscanf(buffer,
+           "cpu  %16llu %16llu %16llu %16llu %16llu %16llu %16llu %16llu %16llu %16llu",
+           &cpuinfo.user, &cpuinfo.nice, &cpuinfo.system, &cpuinfo.idle, &cpuinfo.iowait,
+           &cpuinfo.irq, &cpuinfo.softirq, &cpuinfo.steal, &cpuinfo.guest, &cpuinfo.guest_nice);
+
+    unsigned long long cpu_idle_time = cpuinfo.idle + cpuinfo.iowait;
+    unsigned long long cpu_non_idle_time = cpuinfo.user + cpuinfo.nice + cpuinfo.system + cpuinfo.idle
+                                        + cpuinfo.iowait + cpuinfo.irq + cpuinfo.softirq + cpuinfo.steal;
+
+    unsigned long long cpu_usage_time = cpu_non_idle_time - cpu_idle_time;
+    unsigned long long cpu_percentage = cpu_usage_time / cpu_non_idle_time * 100;    
+
+    asprintf(&result, "%16llu", cpu_percentage);
+    return result;
+}
+
+/**
+ * @brief      Gets the memory usage number from a line in kB
+ * @param      line  The line from /proc/meminfo file
+ * @return     One item from memory usage as number
+ */
 int get_mem_usage_num(char *line)
 {
     char *token;
@@ -65,11 +124,11 @@ int get_mem_usage_num(char *line)
     return result;
 }
 
-char *get_cpu_usage()
-{
-    return NULL;
-}
 
+/**
+ * @brief      Gets the memory usage
+ * @return     The memory usage or NULL in case of error
+ */
 char *get_memory_usage()
 {
     FILE *mem_f;
@@ -80,13 +139,14 @@ char *get_memory_usage()
     char *result;
 
     if ((mem_f = fopen("/proc/meminfo", "r")) == NULL) {
+        perror("Could not open /proc/meminfo file");
         return NULL;
     }
 
     /* Parsing only the first five lines of /proc/meminfo */
     for (int i = 0; i < 5; ++i) {
         if (getline(&line, &len, mem_f) == -1) {
-            fprintf(stderr, "Error on parsing a /proc/meminfo file");
+            perror("Could not read /proc/meminfo file");
             fclose(mem_f);
             return NULL;
         }
@@ -118,9 +178,14 @@ char *get_memory_usage()
     return result;
 }
 
+
+/**
+ * @brief      Parse and execute received command
+ * @param      buffer  Buffer containing received buffer
+ * @return     CPU or memory usage or NULL in case of error
+ */
 char *par_exec_command(char *buffer)
 {
-
     if (strncmp(buffer, "cpu\r", 4) == 0) {
         return get_cpu_usage();
     } else if (strncmp(buffer, "mem\r", 4) == 0) {
@@ -130,6 +195,9 @@ char *par_exec_command(char *buffer)
     return NULL;
 }
 
+/**
+ * @brief      Thread running the server
+ */
 void *server_run()
 {
     int socket_id = connfd;
@@ -138,49 +206,50 @@ void *server_run()
 
     /* Read message from client */
     if (read(socket_id, recv_buffer, sizeof(recv_buffer)) < 0) {
-        fprintf(stderr, "Error on read\n");
+        fprintf(stderr, "Error reading socket\n");
         return NULL;
     }
 
     /* Parse and execute received command */
     char *send_buffer = par_exec_command(recv_buffer);
     if (send_buffer == NULL) {
-        asprintf(&send_buffer, "Invalid command!\n");
+        asprintf(&send_buffer, "Invalid command received!\n");
     }
 
     /* Send a reply to a client */
     if ( write(socket_id, send_buffer, strlen(send_buffer) * sizeof(char) ) < 0 ) {
-        fprintf(stderr, "Error on write\n");
+        fprintf(stderr, "Error writing to socket\n");
         free(send_buffer);
         return NULL;
     }
     free(send_buffer);
     
-    // /* close connection, clean up socket */
+    /* close connection, clean up socket */
     if (close(socket_id) < 0) {
-        fprintf(stderr, "Error on close");
+        fprintf(stderr, "Error closing the socket");
         return NULL;
     }
 
     return NULL;
 }
 
+/**
+ * @brief      Deamonize the running program
+ */
 static void daemonize()
 {
     pid_t pid;
     pid = fork();
-
-    /* An error occurred */
     if (pid < 0) {
         exit(EXIT_FAILURE);
     }
 
-    /* Success: Let the parent terminate */
+    /* On success - terminate parent */
     if (pid > 0) {
         exit(EXIT_SUCCESS);
     }
 
-    /* On success: The child process becomes session leader */
+    /* On success - The child process becomes session leader */
     if (setsid() < 0) {
         exit(EXIT_FAILURE);
     }
@@ -191,28 +260,17 @@ static void daemonize()
 
     /* Fork for the second time */
     pid = fork();
-
-    /* An error occurred */
     if (pid < 0) {
         exit(EXIT_FAILURE);
     }
 
-    /* Success: Let the parent terminate */
+    /* On success - Let the parent terminate */
     if (pid > 0) {
         exit(EXIT_SUCCESS);
     }
 
-    /* Set new file permissions */
-    // umask(0);
-
-    /* Change the working directory to the root directory */
-    /* or another appropriated directory */
-    chdir("/");
-
     /* Close all open file descriptors */
-    int x;
-    for (x = sysconf(_SC_OPEN_MAX); x>=0; x--)
-    {
+    for (int x = sysconf(_SC_OPEN_MAX); x >= 0; x--) {
         close (x);
     }
 
@@ -220,6 +278,14 @@ static void daemonize()
     // openlog ("firstdaemon", LOG_PID, LOG_DAEMON);
 }
 
+
+/**
+ * @brief      Handles a TCP connection properties
+ * @param[in]  port_num  The port number
+ * @param      sin       The sine
+ * @param      listenfd  The listenfd
+ * @return     Returns ERR_OK or error code in case of error
+ */
 int create_connection(const int port_num, struct sockaddr_in *sin, int *listenfd)
 {
     // Create socket
@@ -228,7 +294,7 @@ int create_connection(const int port_num, struct sockaddr_in *sin, int *listenfd
         return ERR_COMM;
     }
     sin->sin_family = PF_INET;              /* set protocol family to Internet */
-    sin->sin_port = htons(port_num);               /* set port no. */
+    sin->sin_port = htons(port_num);        /* set port no. */
     sin->sin_addr.s_addr  = INADDR_ANY;     /* set IP addr to any interface */
 
     // Bind socket
@@ -246,6 +312,12 @@ int create_connection(const int port_num, struct sockaddr_in *sin, int *listenfd
     return ERR_OK;
 }
 
+/**
+ * @brief      Main function
+ * @param[in]  argc  The number of problem agruments
+ * @param      argv  Program arguments
+ * @return     Returns ERR_OK or error code in case of error
+ */
 int main(int argc, char const *argv[])
 {
     unsigned int sinlen;
@@ -254,7 +326,6 @@ int main(int argc, char const *argv[])
     /* Thread variables */
     pthread_t thread_id;
     int status;
-    void *result;
     int res;
     pthread_attr_t attr;
 
